c3d0d66143c916d2316514a3fbbe3cb80e65ff6e
ironc <ironchefjayy@gmail.com>
2025-04-26 20:34:12 -0700

    added styling for navigation and week 10 list_users

--- week10/homework/navigation.php
+++ week10/homework/navigation.php
@@ -1,9 +1,13 @@
 <?php 
     echo '
-    <a href="add_order.php">Add Order Page</a> -
-    <a href="add_product.php">Add Product Page</a> -
-    <a href="add_user.php">Add User Page</a> -
-    <a href="list_orders.php">Order List Page</a> -
-    <a href="list_products.php">Products List Page</a> -
-    <a href="list_users.php">User List Page</a>';
+        <div class="flex flex-wrap justify-around gap-4 text-2xl mt-16 mx-auto">
+            <a href="add_order.php" class="hover:text-blue-500">Add Order Page</a>
+            <a href="add_product.php" class="hover:text-indigo-500">Add Product Page</a>
+            <a href="add_user.php" class="hover:text-green-500">Add User Page</a>
+            <a href="list_orders.php" class="hover:text-blue-500">Order List Page</a>
+            <a href="list_products.php" class="hover:text-indigo-500">Products List Page</a>
+            <a href="list_users.php" class="hover:text-green-500">User List Page</a>
+        </div>
+    ';
+    
 ?>
