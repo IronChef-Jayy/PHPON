49a92f7cc468e9adc2f84e82703151f8e48e4889
ironc <ironchefjayy@gmail.com>
2025-05-02 05:24:50 -0700

    created week12 list user page and edit user page

---
+++ week12/lab/edit_user.php
@@ -0,0 +1,84 @@
+
+
+<?php 
+
+echo "<h1 class='text-6xl text-green-500 mt-28 mb-8 mx-24'>Edit Users</h1>";
+
+$users_id = $_GET['id'];
+// echo $users_id;
+
+require('../../dbconnect.php'); // use require because we want to force this to exist before running our queries
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // check for the post
+        // print_r($_POST);
+
+        // add variables from post
+        $userid = $_POST['userid'];
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $status = $_POST['status'];
+
+        $update_query = "
+            UPDATE users_tbl
+            SET first_name = '$firstname',
+            last_name = '$lastname', 
+            email = '$email',
+            status = '$status'
+            WHERE users_id = '$userid'; 
+        ";
+
+        // echo $update_query;
+
+        // execute update query
+        $update_result = mysqli_query($connection, $update_query);
+
+        if ($update_result) {
+            // success
+            echo "Success";
+            // header("Location: list_users.php?msg=ok");
+            exit;
+            // echo "Record successfully edited"
+        } else {
+            // failure
+            echo "Failed";
+        }
+
+
+    } else {
+        // perform a query and fetch the columns to extract data
+        $query = "SELECT users_id, first_name, last_name, email, status FROM users_tbl WHERE users_id = $users_id";
+
+
+        $result = mysqli_query($connection, $query);
+        $row = mysqli_fetch_array($result);
+
+
+    }
+    
+    
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Users</title>
+</head>
+<body>
+
+    <form action="edit_user.php" method="post">
+        <p>User ID : <input type="text" name="userid" value="<?php echo $row['users_id']; ?>" readonly></p>
+        <p>First Name : <input type="text" name="firstname" value="<?php echo $row['first_name']; ?>"></p>
+        <p>Last Name : <input type="text" name="lastname" value="<?php echo $row['last_name']; ?>"></p>
+        <p>Email : <input type="email" name="email" value="<?php echo $row['email']; ?>"></p>
+        <p>Status : <input type="text" name="status" value="<?php echo $row['status']; ?>"></p>
+
+        <button type="submit">submit</button>
+    </form>
+    
+</body>
+</html>
