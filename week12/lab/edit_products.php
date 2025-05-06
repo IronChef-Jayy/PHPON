354fd5b6daae64884e8b0aa996fed5261cd15c49
ironc <ironchefjayy@gmail.com>
2025-05-06 09:46:55 -0700

    tailwindcss play

--- week12/lab/edit_products.php
+++ week12/lab/edit_products.php
@@ -76,6 +76,7 @@ require('../../dbconnect.php'); // use require because we want to force this to 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Edit Users</title>
 </head>
 <body>
